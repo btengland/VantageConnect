56df337da2fd6926a94e77fed424d758b0e4a602
btengland <bradyeng22@gmail.com>
2025-08-12 16:35:33 -0600

    fixed logo placement

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -90,8 +90,8 @@ const styles = StyleSheet.create({
   },
   discordButton: {
     position: 'absolute',
-    top: 55,
-    right: 30,
+    top: 50,
+    right: 20,
     zIndex: 10,
   },
   discordLogo: {
